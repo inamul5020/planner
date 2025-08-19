bad85669c2f9fe5c2a4c1f823527557cfd41a156
Frappe User <frappeuser@instance-20250810-133016.us-central1-c.c.ambient-scope-462704-u2.internal>
2025-08-19 06:16:15 +0000

    added doctypes scaffold

---
+++ planner/planner/doctype/isp_payment/isp_payment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, YOUR COMPANY / NAME and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("ISP Payment", {
+// 	refresh(frm) {
+
+// 	},
+// });
